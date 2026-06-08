a2d8ce27fec52c9e2f779968c0ccc99b12296c4a
Piotr <57483126+piotroonio@users.noreply.github.com>
2026-06-08 15:56:47 +0200

    Update fetch_outside.js

--- scripts/fetch_outside.js
+++ scripts/fetch_outside.js
@@ -128,13 +128,7 @@ async function processUser(user, CLIENT_ID, CLIENT_SECRET) {
   const [lat, lng] = a.start_latlng;
   const [endLat, endLng] = a.end_latlng; // nowe
   
-  // const mapLink = `https://www.openstreetmap.org/directions?engine=fossgis_osrm_bike` + `&route=${lat}%2C${lng}%3B${endLat}%2C${endLng}`; Wytyczanie trasy
-  
-  const mapLink =
-    `https://www.openstreetmap.org/?` +
-    `mlat=${lat}&mlon=${lng}` +
-    `&#map=12/${lat}/${lng}` +
-    `&markers=${lat},${lng},red;${endLat},${endLng},blue`;
+  const mapLink = `https://www.openstreetmap.org/directions?engine=fossgis_osrm_bike` + `&route=${lat}%2C${lng}%3B${endLat}%2C${endLng}`; Wytyczanie trasy
 
   const dist = getDistance(lat, lng, CITY.lat, CITY.lng);
 
